a020f8266292ea94fd40f62b179ee208d8c0f6ad
tbagwill <tylerray.bagwill@calbaptist.edu>
2022-12-06 14:00:48 -0800

    readded firebase config

---
+++ fbconfig.js
@@ -0,0 +1,34 @@
+import {
+    PUBLIC_API_KEY,
+    PUBLIC_AUTH_DOMAIN,
+    PUBLIC_PROJECT_ID,
+    PUBLIC_STORAGE_BUCKET,
+    PUBLIC_MESSAGING_SENDER_ID,
+    PUBLIC_APP_ID,
+    PUBLIC_MEASUREMENT_ID
+} from '$env/static/public';
+
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: `${PUBLIC_API_KEY}`,
+    authDomain: `${PUBLIC_AUTH_DOMAIN}`,
+    projectId: `${PUBLIC_PROJECT_ID}`,
+    storageBucket: `${PUBLIC_STORAGE_BUCKET}`,
+    messagingSenderId: `${PUBLIC_MESSAGING_SENDER_ID}`,
+    appId: `${PUBLIC_APP_ID}`,
+    measurementId: `${PUBLIC_MEASUREMENT_ID}`
+};
+
+const initAppIfNeeded = () => {
+    try {
+        return getApp('containerappserver');
+    } catch {
+        return initializeApp(firebaseConfig, 'containerappserver');
+    }
+};
+
+let firebaseapp = initAppIfNeeded();
+
+export const db = getFirestore(firebaseapp);
