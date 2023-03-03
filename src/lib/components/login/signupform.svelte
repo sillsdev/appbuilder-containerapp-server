2244165c0bf799c8ae2caac9f568c4b76701368f
Ishan Dhillon <70235067+ishan510@users.noreply.github.com>
2023-03-03 11:00:46 -0800

    add basic daisyui admin layout

---
+++ src/lib/components/login/signupform.svelte
@@ -0,0 +1,47 @@
+<script>
+    import { Form, TextInput, PasswordInput, Button } from 'carbon-components-svelte';
+    import { createEventDispatcher } from 'svelte';
+    let dispatch = createEventDispatcher();
+    let username, email, password;
+
+    function signup() {
+        dispatch('signup', {
+            username,
+            email,
+            password
+        });
+    }
+</script>
+
+<div class="form">
+    <Form>
+        <div class="space" />
+        <TextInput
+            bind:value={email}
+            labelText="Email address"
+            placeholder="Enter your email"
+            type="email"
+            name="email"
+        />
+        <div class="space" />
+        <PasswordInput
+            bind:value={password}
+            tooltipAlignment="start"
+            tooltipPosition="left"
+            labelText="Password"
+            placeholder="Enter password"
+            name="password"
+        />
+        <div class="space" />
+        <Button size="small" on:click={signup}>Sign Up</Button>
+    </Form>
+</div>
+
+<style>
+    .form {
+        width: 30%;
+    }
+    .form .space {
+        margin: 20px 0;
+    }
+</style>
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,3 +1,69 @@
-<script></script>
+<html data-theme="light" />
+<div class="drawer drawer-mobile">
+    <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content flex flex-col items-center justify-center">
+        <!-- Page content here -->
+        <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+    </div>
+    <div class="drawer-side">
+        <label for="my-drawer-2" class="drawer-overlay" />
+        <ul class="menu p-4 w-80 bg-base-100 text-base-content">
+            <!-- Sidebar content here -->
+            <li><a>Sidebar Item 1</a></li>
+            <li><a>Sidebar Item 2</a></li>
+        </ul>
+    </div>
+</div>
+<div class="vl" />
+<div class="drawer">
+    <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content flex flex-col">
+        <!-- Navbar -->
+        <div class="w-full navbar bg-base-300">
+            <div class="flex-none lg:hidden">
+                <label for="my-drawer-3" class="btn btn-square btn-ghost">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        class="inline-block w-6 h-6 stroke-current"
+                        ><path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16"
+                        /></svg
+                    >
+                </label>
+            </div>
+            <div class="flex-1 px-2 mx-2">Sciripture App Builder</div>
+            <div class="flex-none hidden lg:block">
+                <ul class="menu menu-horizontal">
+                    <!-- Navbar menu content here -->
+                    <li><a>Navbar Item 1</a></li>
+                    <li><a>Navbar Item 2</a></li>
+                </ul>
+            </div>
+        </div>
+        <!-- Page content here -->
+        Content
+    </div>
+</div>
 
-<h1>Welcome to admin</h1>
+<style>
+    .drawer-side {
+        position: absolute;
+        left: 0%;
+        margin-top: 64px;
+    }
+    .drawer {
+        position: absolute;
+        width: 100%;
+    }
+    .vl {
+        position: absolute;
+        border-left: 1px solid black;
+        height: 100%;
+        left: 19%;
+    }
+</style>
