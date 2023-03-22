5e3e9df005bd28285a79c2a0e40fdba7b15d7d38
Ishan Dhillon <70235067+ishan510@users.noreply.github.com>
2023-03-21 18:04:18 -0700

    ran npm i and changed the avatar to "..."

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -37,9 +37,20 @@
         </div>
         <div class="flex-none">
             <div class="dropdown dropdown-end">
-                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                    <div class="w-10 rounded-full" />
-                </label>
+                <button class="btn btn-square btn-ghost">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        class="inline-block w-5 h-5 stroke-current"
+                        ><path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                        /></svg
+                    >
+                </button>
                 <ul
                     tabindex="0"
                     class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
