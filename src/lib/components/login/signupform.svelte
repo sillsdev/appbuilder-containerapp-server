1bb7e253262223188990a2c53afd1bfc3057f283
Ishan Dhillon <70235067+ishan510@users.noreply.github.com>
2023-03-30 12:39:26 -0700

    initials and password visibility  (#12)
    * add basic daisyui admin layout
    
    * admin page
    
    * prettier fix
    
    * fix realtive path imports
    
    instead of ../../lib/fbconfig switched too $lib/...
    
    * removed the avatar image
    
    * ran npm i and changed the avatar to "..."
    
    * initials and password visibility
    
    changed avatar to have user's initials
    
    floated the initials icon to the right
    
    added show/hide password visibility to login and signup page
    
    * ran prettier
    
    ran prettier on userInitialsStore.js

--- src/lib/components/login/signupform.svelte
+++ src/lib/components/login/signupform.svelte
@@ -1,18 +1,52 @@
 <script>
     import { createEventDispatcher } from 'svelte';
     let dispatch = createEventDispatcher();
-    let username, email, password;
+    let firstname,
+        lastname,
+        email,
+        password,
+        showPassword = false;
 
     function signup() {
         dispatch('signup', {
-            username,
+            firstname,
+            lastname,
             email,
             password
         });
     }
+
+    function togglePasswordVisibility() {
+        const passwordInput = document.querySelector('#password-input');
+        showPassword = !showPassword;
+
+        if (showPassword) {
+            passwordInput.type = 'text';
+        } else {
+            passwordInput.type = 'password';
+        }
+    }
 </script>
 
 <div class="form">
+    <div class="space" />
+    <input
+        class="input input-bordered w-full max-w-xs"
+        bind:value={firstname}
+        type="firstname"
+        labelText="FirstName"
+        placeholder="Enter Your First Name"
+        name="firstname"
+    />
+    <div class="space" />
+    <input
+        class="input input-bordered w-full max-w-xs"
+        bind:value={lastname}
+        type="lastname"
+        labelText="LastName"
+        placeholder="Enter Your Last Name"
+        name="lastname"
+    />
     <div class="space" />
     <input
         class="input input-bordered w-full max-w-xs"
@@ -25,7 +59,9 @@
     <div class="space" />
     <input
         class="input input-bordered w-full max-w-xs"
+        id="password-input"
         labelText="Password"
+        type="password"
         bind:value={password}
         placeholder="Enter password"
         tooltipAlignment="start"
@@ -35,6 +71,11 @@
     <div class="space" />
     <button class="btn" on:click={signup}>Sign Up</button>
 </div>
+<div class="psbtn">
+    <button class="btn btn-xs" on:click={togglePasswordVisibility}>
+        {showPassword ? 'Hide Password' : 'Show Password'}
+    </button>
+</div>
 
 <style>
     .form {
@@ -42,5 +83,11 @@
     }
     .form .space {
         margin: 20px 0;
+    }
+
+    .psbtn {
+        position: absolute;
+        top: 56%;
+        right: 38%;
     }
 </style>
