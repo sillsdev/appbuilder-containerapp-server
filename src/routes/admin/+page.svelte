f415a87be56202f523262e5a9587b0fe811ab80e
Ishan Dhillon <70235067+ishan510@users.noreply.github.com>
2023-04-10 14:42:42 -0700

    Update +page.svelte

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -84,7 +84,7 @@
                 </button>
                 <ul
                     tabindex="0"
-                    class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 "
+                    class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
                 >
                     <li><a>Settings</a></li>
                     <li><button on:click={logOut}>Sign Out</button></li>
