0346b476eb91239fd3ca96fc0b384ac013efcf2c
tbagwill <tylerray.bagwill@calbaptist.edu>
2023-04-11 13:14:51 -0700

    changed data load to layout.ts

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,10 +2,13 @@ import { browser } from '$app/environment';
 import '$lib/i18n';
 import { locale, waitLocale } from 'svelte-i18n';
 import type { LayoutLoad } from './$types';
+import { initPackages } from '../stores/packstore';
 
 export const load: LayoutLoad = async () => {
     if (browser) {
         locale.set(window.navigator.language);
     }
     await waitLocale();
+
+    await initPackages();
 };
