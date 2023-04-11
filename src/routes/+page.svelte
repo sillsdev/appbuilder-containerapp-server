a4f7be63a625fc16a6cc44fc12005b0ad89f5aa3
tbagwill <tylerray.bagwill@calbaptist.edu>
2023-04-11 16:02:21 -0700

    main page adjustments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script>
+    import { _ } from 'svelte-i18n';
+    import Icon from '@iconify/svelte';
     import { packages } from '../stores/packstore';
     import PackageCard from '../components/PackageCard.svelte';
-    import { _ } from 'svelte-i18n';
     import LanguageSelector from '../components/LanguageSelector.svelte';
 
     let searchName = '';
@@ -39,17 +40,21 @@
 
 <head> Scripture App Builder </head>
 
-<section id="main">
+<section id="header">
+    <div class="btn btn-ghost m-1 p-2">
+        <a href="https://software.sil.org/scriptureappbuilder/">
+            <Icon icon="ph:info-bold" color="white" width="32" />
+        </a>
+    </div>
+
     <div class="title">{$_('page.home.title')}</div>
 
     <div id="language_selector">
         <LanguageSelector />
     </div>
+</section>
 
-    <div class="button">
-        <a href="https://google.com">{$_('page.home.about')}</a>
-    </div>
-
+<section id="main">
     <div class="search-box">
         {#if !searchCountry && !searchCode}
             <div class="search">
@@ -78,33 +83,19 @@
 </section>
 
 <style>
-    #language_selector {
-        position: absolute;
-        top: 3%;
-        left: 90%;
+    #header {
+        display: flex;
     }
 
-    .search-box {
-        display: flex;
-        flex-direction: column;
-        -ms-flex-align: center;
-        width: 100%;
-        margin-top: 5%;
-    }
     a {
         color: white;
-    }
-    .button {
-        color: white;
-        position: absolute;
-        top: 4%;
-        left: 10%;
     }
 
     #main {
         width: 100%;
         height: 1000px;
         display: flex;
+        align-items: center;
         flex-direction: column;
         background-position: center;
         background-size: cover;
@@ -128,6 +119,14 @@
         margin: 8px;
         margin-top: 4%;
         padding: 6px;
+    }
+
+    .search-box {
+        display: flex;
+        flex-direction: column;
+        -ms-flex-align: center;
+        width: 100%;
+        margin-top: 5%;
     }
 
     .search {
