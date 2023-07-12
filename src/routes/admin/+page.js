07fefec04dd65209c5410899e9a0aa01af30200e
tbagwill <tylerray.bagwill@calbaptist.edu>
2023-07-12 13:40:49 -0700

    empty load fn

--- src/routes/admin/+page.js
+++ src/routes/admin/+page.js
@@ -1,23 +1,3 @@
-import { db } from '$lib/fbconfig.js';
-import { collection, query, where, getDocs } from 'firebase/firestore';
-
-export async function load({ depends }) {
-    const activeRef = collection(db, 'packages');
-    const activeQ = query(activeRef, where('accepted', '!=', ''));
-    const activeQuery = await getDocs(activeQ);
-    const active = activeQuery.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    // console.log(`Loaded ${active.length} active packages `);
-
-    const inactiveRef = collection(db, 'packages');
-    const inactiveQ = query(inactiveRef, where('accepted', '==', ''));
-    const inactiveQuery = await getDocs(inactiveQ);
-    const inactive = inactiveQuery.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    // console.log(`Loaded ${inactive.length} inactive packages `);
-
-    depends('packages');
-
-    return {
-        active,
-        inactive
-    };
+export async function load() {
+    return {};
 }
