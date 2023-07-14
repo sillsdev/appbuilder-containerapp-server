783b7b31c487157d014578005e1d42fc6680925f
tbagwill <tylerray.bagwill@calbaptist.edu>
2023-07-14 14:32:18 -0700

    formatting

--- src/lib/stores/users.js
+++ src/lib/stores/users.js
@@ -1,6 +1,6 @@
-import { writable } from "svelte/store";
-import { db } from '$lib/fbconfig.js'
-import { collection, getDocs } from "firebase/firestore";
+import { writable } from 'svelte/store';
+import { db } from '$lib/fbconfig.js';
+import { collection, getDocs } from 'firebase/firestore';
 
 export const allUsers = writable([]);
 export const userInitials = writable([]);
