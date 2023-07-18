4cf44629904ec50cf61540b5356c3b75fbef4c83
tbagwill <tylerray.bagwill@calbaptist.edu>
2023-07-18 14:25:50 -0700

    formatting for lint

--- src/routes/submit/+server.js
+++ src/routes/submit/+server.js
@@ -3,33 +3,33 @@ import { addDoc, collection } from 'firebase/firestore';
 import { allKeys, initKeys } from '$lib/stores/keys.js';
 import { initPackages } from '$lib/stores/packages.js';
 
-export async function POST( {request} ) {
+export async function POST({ request }) {
     await initKeys();
 
     let valid = false;
 
     const bearer = request.headers.get('authorization');
 
-    allKeys.subscribe( keys => {
-        keys.forEach(key => {
-            if( bearer.includes( key.key ) ){
+    allKeys.subscribe((keys) => {
+        keys.forEach((key) => {
+            if (bearer.includes(key.key)) {
                 valid = true;
-            } 
-        })
+            }
+        });
     });
 
-    if(valid){
+    if (valid) {
         const pack = await request.json();
-        
+
         const col = collection(db, 'packages');
 
-        await addDoc( col, {
+        await addDoc(col, {
             accepted: '',
             ...pack
         });
 
         await initPackages();
-        
+
         return new Response(201);
     } else {
         return new Response(401);
