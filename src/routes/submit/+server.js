2f68f985ab5d84bd6d0e0badf09b24bf10bf61ed
Ty Bagwill <tylerray.bagwill@calbaptist.edu>
2023-07-19 13:41:54 -0700

    Feature/Rest API (#39)
    * added REST endpoint
    
    * rest api POST route
    
    * removed redundancy in post method
    
    * formatting for lint
    
    * removed additional redundancy

---
+++ src/routes/submit/+server.js
@@ -0,0 +1,37 @@
+import { db } from '$lib/fbconfig.js';
+import { addDoc, collection } from 'firebase/firestore';
+import { allKeys, initKeys } from '$lib/stores/keys.js';
+import { initPackages } from '$lib/stores/packages.js';
+
+export async function POST({ request }) {
+    await initKeys();
+
+    let valid = false;
+
+    const bearer = request.headers.get('authorization');
+
+    allKeys.subscribe((keys) => {
+        keys.forEach((key) => {
+            if (bearer.includes(key.key)) {
+                valid = true;
+            }
+        });
+    });
+
+    if (valid) {
+        const pack = await request.json();
+
+        const col = collection(db, 'packages');
+
+        await addDoc(col, {
+            accepted: '',
+            ...pack
+        });
+
+        await initPackages();
+
+        return new Response(201);
+    } else {
+        return new Response(401);
+    }
+}
